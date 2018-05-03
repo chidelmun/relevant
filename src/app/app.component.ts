4531d58a69cbb220d6c056ae51e640d0e4dec760
Delveri Chick <chickdelveri@gmail.com>
2018-05-03 02:27:14 -0400

    Saved dynamic classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,45 @@ declare const fabric: any;
 })
 
 export class AppComponent {
+  price: number = 0;
+  activeIndex = 0;
+  classes = ['col-left-green', 
+              'col-left-orange',
+              'col-left-yellow',
+              'col-left-olive',
+              'col-left-pink'
+            ];
+  activeClass = this.classes[this.activeIndex];
 
   constructor() { }
 
   ngOnInit() {
 
     //setup front side canvas
+  }
+
+  addItemToCart() {
+    this.price += 1;
+  }
+  nextClassIndex(){
+    console.log(this.activeIndex);
+    if(this.activeIndex > 3){
+      this.activeIndex = 0;
+    }else{
+      this.activeIndex +=1;
+    }
+    this.activeClass = this.classes[this.activeIndex];
+  }
+
+  prevClassIndex(){
+    console.log(this.activeIndex);
+    console.log('Hello')
+    if(this.activeIndex < 1){
+      this.activeIndex = 4;
+    }else{
+      this.activeIndex -=1;
+    }
+    this.activeClass = this.classes[this.activeIndex];
   }
 
 }
