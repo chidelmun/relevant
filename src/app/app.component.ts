7e3369f8849b56a9119453894d19ba0ad8da4f2c
Delveri Chick <chickdelveri@gmail.com>
2018-05-03 20:26:38 -0400

    Saved changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class AppComponent {
     handler.open({
       name: 'Charmaine Aprons Checkout',
       description: 'White Apron',
-      amount: this.itemPrice * this.price
+      amount: this.itemPrice * this.price * 100
     });
   }
 
